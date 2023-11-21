d162185c2266821331a8ac8113c52f320de812f1
shawn edgell <shawn_edgell@hotmail.com>
2023-11-20 22:00:27 -0500

    added objects

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
-# Basic Three.js Project
+# Three.js Interactive 3D Scene
 
 ## Overview
-This repository contains a simple Three.js project, demonstrating the basic setup required to create a 3D scene with a rotating cube. It's an excellent starting point for anyone new to Three.js and 3D web graphics.
+This project is a simple but interactive 3D scene built using Three.js. It showcases basic features of Three.js, including creating 3D geometries, adding lighting, and enabling user interaction.
+
+## Features
+- **Interactive 3D Objects**: The scene contains a cube, a sphere, and a torus, each of which can change color upon mouse click.
+- **Animations**: The cube and torus rotate continuously, while the sphere has a bouncing animation.
+- **Responsive Design**: The scene adjusts to the browser window's size.
+- **Lighting**: Basic ambient and point lighting are included to enhance the visuals.
+
+## Setup
+To run this project locally, follow these steps:
+1. Clone the repository to your local machine.
+2. Open the `index.html` file in a modern web browser.
+3. Click on the 3D objects to change their colors. Observe the animations and responsive behavior.
+
+## Technologies Used
+- HTML
+- JavaScript
+- Three.js (WebGL)
+
+## Possible Enhancements
+- Implement advanced animations and transitions.
+- Add more interactive elements and complex geometries.
+- Integrate physics for realistic object behavior.
+- Add a user interface for controlling scene parameters.
--- app.js
+++ app.js
@@ -2,15 +2,15 @@
 var scene = new THREE.Scene();
 
 // Camera
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 10;
 
 // Renderer
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Resize listener
+// Resize Event Listener
 window.addEventListener('resize', () => {
     var width = window.innerWidth;
     var height = window.innerHeight;
@@ -25,11 +25,63 @@ var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-// Animation
+// Sphere
+var sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+var sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.position.x = -3;
+scene.add(sphere);
+
+// Torus
+var torusGeometry = new THREE.TorusGeometry(1, 0.4, 16, 100);
+var torusMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+var torus = new THREE.Mesh(torusGeometry, torusMaterial);
+torus.position.x = 3;
+scene.add(torus);
+
+// Lighting
+var ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+var pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.position.set(5, 10, 5);
+scene.add(pointLight);
+
+// Raycaster for mouse interaction
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+
+function onMouseClick(event) {
+    event.preventDefault();
+
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+
+    raycaster.setFromCamera(mouse, camera);
+
+    var intersects = raycaster.intersectObjects(scene.children);
+
+    for (var i = 0; i < intersects.length; i++) {
+        intersects[i].object.material.color.set(Math.random() * 0xffffff);
+    }
+}
+
+window.addEventListener('click', onMouseClick, false);
+
+// Animation Function
 function animate() {
     requestAnimationFrame(animate);
+
+    // Rotating the cube and torus
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
+    torus.rotation.y += 0.01;
+
+    // Bouncing sphere
+    sphere.position.y = Math.abs(Math.sin(Date.now() * 0.002)) * 2;
+
+    // Render the scene
     renderer.render(scene, camera);
 }
+
 animate();
